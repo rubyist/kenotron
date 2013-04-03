27dd626c0dad2c6c2a2f9f104616f73889dd38e8
Scott Barron <scott@elitists.net>
2013-04-03 19:36:04 -0400

    Solve button that does nothing yet

--- app/controllers/ken_ken_designer_controller.rb
+++ app/controllers/ken_ken_designer_controller.rb
@@ -5,13 +5,22 @@ class KenKenDesignerController < UIViewController
     
     @grid_view = KenKenGridView.alloc.initWithGridSize(4)
 
+    @solve_button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    @solve_button.setTitle("Solve", forState:UIControlStateNormal)
+    @solve_button.addTarget(self, action:"solve_puzzle", forControlEvents:UIControlEventTouchUpInside)
+
     # No idea how to make this work right. I want it centered horizontally
     Motion::Layout.new do |layout|
       layout.view view
-      layout.subviews "grid" => @grid_view
+      layout.subviews "grid" => @grid_view, "solve" => @solve_button
       layout.metrics "top" => 90, "margin" => 90, "height" => @grid_view.frame.height, "width" => @grid_view.frame.width
-      layout.vertical "|-(top)-[grid(==height)]"
+      layout.vertical "|-(top)-[grid(==height)]-[solve]"
       layout.horizontal "|-(margin)-[grid(==width)]"
+      layout.horizontal "|-(margin)-[solve(==grid)]"
     end
+  end
+  
+  def solve_puzzle
+    puts "Let's solve it!"
   end
 end
